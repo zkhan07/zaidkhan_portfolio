6d551e1e2161d5f2de9c02619f2a06d59ad3c09d
zaid <khanzaid786786.kz@gmail.com>
2020-01-07 14:29:23 +0530

    added

--- src/index.js
+++ src/index.js
@@ -2,26 +2,25 @@ import React from  "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Route} from 'react-router-dom';
 
-import Dashboard from "./components/Dashboard";
-import DashPortfolio from "./components/DashPortfolio";
-import DashOffer from "./components/DashOffer";
-import DashContact from "./components/DashContact";
-import DashMycv from "./components/DashMycv";
-import DashTechtalks from "./components/DashTechtalks";
-import DashBlog from "./components/DashBlog";
+import App from "./components/App";
+
+
+import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
 
 <BrowserRouter>
 
-    <Route exact path="/" component={Dashboard}/>
-    <Route exact path="/portfolio" component={DashPortfolio}/>
-    <Route exact path="/myoffer" component={DashOffer}/>
-    <Route exact path="/mycontact" component={DashContact}/>
-    <Route exact path="/mycv" component={DashMycv}/>
-    <Route exact path="/mytechtalks" component={DashTechtalks}/>
-    <Route exact path="/myblog" component={DashBlog}/>
+    <Route exact path="/" component={App}/>
+  
 
 </BrowserRouter>,
 document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+
