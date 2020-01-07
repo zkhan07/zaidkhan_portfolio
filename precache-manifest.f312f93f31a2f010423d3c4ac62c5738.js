028abcb43bf83a1ee3ea4639200f19424cb72833
zaid <khanzaid786786.kz@gmail.com>
2020-01-07 13:49:03 +0530

    Updates

---
+++ precache-manifest.f312f93f31a2f010423d3c4ac62c5738.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8d25f7c76dff3f9772ac402f975f7b",
+    "url": "/zaidkhan_portfolio/index.html"
+  },
+  {
+    "revision": "70ab2c3ff69967d83dd5",
+    "url": "/zaidkhan_portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8d371f3a2c4826270312",
+    "url": "/zaidkhan_portfolio/static/js/2.b4cbc682.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/zaidkhan_portfolio/static/js/2.b4cbc682.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70ab2c3ff69967d83dd5",
+    "url": "/zaidkhan_portfolio/static/js/main.0ee7bf93.chunk.js"
+  },
+  {
+    "revision": "1f0b769a52547274bc20",
+    "url": "/zaidkhan_portfolio/static/js/runtime-main.2792c41b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zaidkhan_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
