5a5506d05a08ddfa7a7ae90e49f030e599d95bb4
zaid <khanzaid786786.kz@gmail.com>
2020-01-10 14:53:06 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+// import {BrowserRouter, Route} from "react-router-dom";
 
 import Dashboard from "./Dashboard";
 // import DashPortfolio from "./DashPortfolio";
@@ -14,21 +14,23 @@ import "./Style.css";
 
 
 
+
+
 function App() {
   return (
     <div className="container">
 
-        <BrowserRouter>
+        <Dashboard />
 
+        {/* <BrowserRouter>
         <Route exact path="/" component={Dashboard}/>
-        {/* <Route exact path="/portfolio" component={DashPortfolio}/>
+        <Route exact path="/portfolio" component={DashPortfolio}/>
         <Route exact path="/myoffer" component={DashOffer}/>
         <Route exact path="/mycontact" component={DashContact}/>
         <Route exact path="/mycv" component={DashMycv}/>
         <Route exact path="/mytechtalks" component={DashTechtalks}/>
-        <Route exact path="/myblog" component={DashBlog}/> */}
-
-        </BrowserRouter>,
+        <Route exact path="/myblog" component={DashBlog}/>
+        </BrowserRouter>, */}
      
     </div>
   );
