5f6bfd87617c0a6ef3544db8453627e41788eb02
zaid <khanzaid786786.kz@gmail.com>
2020-01-10 15:34:06 +0530

    Updates

--- precache-manifest.3824e4b46a2511f4853b2a2ba59711e3.js
+++ precache-manifest.3824e4b46a2511f4853b2a2ba59711e3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8426874ed7352e9ba557d81424282b5a",
+    "revision": "00565f5bd380bfd62b1da90fdbcd4ed9",
     "url": "/zaidkhan_portfolio/index.html"
   },
   {
-    "revision": "fbc04a11a4686d5fbff2",
-    "url": "/zaidkhan_portfolio/static/css/main.a7c24e8a.chunk.css"
+    "revision": "b7b1a7fdea9db5f5fe35",
+    "url": "/zaidkhan_portfolio/static/css/main.58ceae8f.chunk.css"
   },
   {
     "revision": "65dda5121065becf8c90",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/zaidkhan_portfolio/static/js/2.56d41813.chunk.js.LICENSE"
   },
   {
-    "revision": "fbc04a11a4686d5fbff2",
+    "revision": "b7b1a7fdea9db5f5fe35",
     "url": "/zaidkhan_portfolio/static/js/main.adf6359b.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zaidkhan_portfolio/precache-manifest.76ea478d326e0df5ccdde196ca89fc31.js"
+  "/zaidkhan_portfolio/precache-manifest.3824e4b46a2511f4853b2a2ba59711e3.js"
 );
 
 self.addEventListener('message', (event) => {
