9ec7e25019e5a233d706a5e6f7a4d25ccc29a0b7
zaid <khanzaid786786.kz@gmail.com>
2020-01-10 14:48:28 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
 
 import Dashboard from "./Dashboard";
-import DashPortfolio from "./DashPortfolio";
-import DashOffer from "./DashOffer";
-import DashContact from "./DashContact";
-import DashMycv from "./DashMycv";
-import DashTechtalks from "./DashTechtalks";
-import DashBlog from "./DashBlog";
+// import DashPortfolio from "./DashPortfolio";
+// import DashOffer from "./DashOffer";
+// import DashContact from "./DashContact";
+// import DashMycv from "./DashMycv";
+// import DashTechtalks from "./DashTechtalks";
+// import DashBlog from "./DashBlog";
 import "./Style.css";
 
 
@@ -21,12 +21,12 @@ function App() {
         <BrowserRouter>
 
         <Route exact path="/" component={Dashboard}/>
-        <Route exact path="/portfolio" component={DashPortfolio}/>
+        {/* <Route exact path="/portfolio" component={DashPortfolio}/>
         <Route exact path="/myoffer" component={DashOffer}/>
         <Route exact path="/mycontact" component={DashContact}/>
         <Route exact path="/mycv" component={DashMycv}/>
         <Route exact path="/mytechtalks" component={DashTechtalks}/>
-        <Route exact path="/myblog" component={DashBlog}/>
+        <Route exact path="/myblog" component={DashBlog}/> */}
 
         </BrowserRouter>,
      
